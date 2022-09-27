3a66bced50290fa08fec6ea31307cfd7b963232d
Harriet Landa <hlandagil@gmail.com>
2022-09-27 15:01:58 +0200

    add some routes

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+@app.route('/info/<user>')
+def info(user):
+    return "Hello: " + user
+
+
+@app.route('/post/<post_id>')
+def post(post_id):
+    return "Post Number: " + post_id
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
